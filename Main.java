e1a60c3c66ea1b2643de4a64985eec23abd9c49e
Sapriadi <147253388+Sapriadiadi@users.noreply.github.com>
2023-11-02 21:27:14 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+package days25;
+
+public class Main {
+    
+    
+        
+
+
+    
+
+
+public static void main(String[] args) {
+    int x;
+     x=5;
+System.out.println(x);
+}
+}
+    
+    
